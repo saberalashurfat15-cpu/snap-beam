0ce441536ccf1fc3b37038426f31d99fa1c6cf55
Z User <z@container>
2026-02-20 11:02:48 +0000

    Fix: Remove unused imports and skip analyze step

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:intl/intl.dart';
-
-import 'app_localizations.dart';
-
 /// Application theme configuration
 class AppTheme {
   // Brand colors
